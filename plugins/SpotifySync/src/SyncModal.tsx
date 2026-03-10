aaf6712fc67a3b9c8fccac1e20ac7ebf3666b803
ilya <ilya@cow.fi>
2026-03-10 20:54:41 +0000

    Better copy button

--- plugins/SpotifySync/src/SyncModal.tsx
+++ plugins/SpotifySync/src/SyncModal.tsx
@@ -42,17 +42,26 @@ const TrackList = ({ label, tracks, color, copyable }: { label: string; tracks: 
 				{copyable && (
 					<button
 						onClick={handleCopy}
+						title="Copy to clipboard"
 						style={{
-							padding: "1px 8px",
-							borderRadius: "3px",
-							border: "1px solid rgba(255,255,255,0.15)",
-							background: "transparent",
+							padding: "1px 4px",
+							background: "none",
+							border: "none",
 							color: copied ? "rgba(29,185,84,0.8)" : "rgba(255,255,255,0.5)",
 							cursor: "pointer",
-							fontSize: "11px",
+							verticalAlign: "middle",
 						}}
 					>
-						{copied ? "Copied!" : "Copy"}
+						{copied ? (
+							<svg width="12" height="12" viewBox="0 0 16 16" fill="currentColor">
+								<path d="M13.78 4.22a.75.75 0 010 1.06l-7.25 7.25a.75.75 0 01-1.06 0L2.22 9.28a.75.75 0 011.06-1.06L6 10.94l6.72-6.72a.75.75 0 011.06 0z" />
+							</svg>
+						) : (
+							<svg width="12" height="12" viewBox="0 0 16 16" fill="currentColor">
+								<path d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 010 1.5h-1.5a.25.25 0 00-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 00.25-.25v-1.5a.75.75 0 011.5 0v1.5A1.75 1.75 0 019.25 16h-7.5A1.75 1.75 0 010 14.25z" />
+								<path d="M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0114.25 11h-7.5A1.75 1.75 0 015 9.25zm1.75-.25a.25.25 0 00-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 00.25-.25v-7.5a.25.25 0 00-.25-.25z" />
+							</svg>
+						)}
 					</button>
 				)}
 			</span>
